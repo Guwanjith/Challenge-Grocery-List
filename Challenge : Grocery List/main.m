4480bf34bbc7b0d606d1c80c8835bef586e07e26
Guwanjith <guwanjithtennekoon@Guwanjiths-MacBook-Pro.local>
2014-09-03 23:02:57 +0530

    new

--- Challenge : Grocery List/main.m
+++ Challenge : Grocery List/main.m
@@ -13,8 +13,21 @@ int main(int argc, const char * argv[])
 
     @autoreleasepool {
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        //Create an empty mutable array
+        NSMutableArray *groceryList = [NSMutableArray array];
+        
+        NSString *bread = @"Loaf of bread";
+        NSString *milk = @"Container of milk" ;
+        NSString *butter = @"Stick of butter";
+        //Add groceries
+        [groceryList addObject: bread];
+        [groceryList addObject:milk];
+        [groceryList addObject:butter];
+        
+        NSLog(@"My grocery list is:\n");
+        for(NSString *s in groceryList){
+            NSLog(@"%@\n", s);
+        }
         
     }
     return 0;
